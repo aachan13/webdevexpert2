19409e24c9dec4e2c094c60e2288dca4b5d0ee43
Achmad Fadhil <afadhilprtm@gmail.com>
2021-06-25 20:04:54 +0800

    idb + config

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -2,6 +2,11 @@ const CONFIG = {
   KEY: 'YOUR_API_KEY',
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_MED_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  BASE_LARGE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'foodex-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'foodrestaurants',
 };   
 
 export default CONFIG;
