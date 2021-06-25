627033fbb4d349aa9f209c888e82d42fd3a00fa6
Achmad Fadhil <afadhilprtm@gmail.com>
2021-06-25 20:08:29 +0800

    halaman favorite

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,12 +1,25 @@
+import FoodexIdb from '../../data/foodex-idb';
+import { createFoodRestItemTemplate } from '../templates/template-creator';
+
 const Favorite = {
   async render() {
     return `
-            <h2>Favorite page</h2>
+            <h1 class="text-center page-label">Your Favorite Restaurant</h1>
+
+            <div id="restaurantFavContainer" class="main-content">
+              <div class="loader"><div></div><div></div></div>
+        
+            </div>
           `;
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const foodRestaurant = await FoodexIdb.getAllFoodRests();
+    const restaurantItemContainer = document.querySelector('#restaurantFavContainer');
+    restaurantItemContainer.innerHTML = '';
+    foodRestaurant.forEach((restaurant) => {
+      restaurantItemContainer.innerHTML += createFoodRestItemTemplate(restaurant);
+    });
   },
 };
 
