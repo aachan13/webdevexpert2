9b8231d23a8a92fe8d9b94407defe7011fa615d9
Achmad Fadhil <afadhilprtm@gmail.com>
2022-03-31 13:05:22 +0800

    perbaikan url

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   KEY: 'YOUR_API_KEY',
-  BASE_URL: 'https://restaurant-api.dicoding.dev',
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_MED_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
   BASE_LARGE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
   CACHE_NAME: new Date().toISOString(),
