c3cb7c98d9562bab97fa30e5d084db5e877e440d
Achmad Fadhil <afadhilprtm@gmail.com>
2022-03-31 13:06:21 +0800

    perbaikan url

--- src/scripts/global/api-endpoint.js
+++ src/scripts/global/api-endpoint.js
@@ -1,7 +1,7 @@
 import CONFIG from './config';
  
 const API_ENDPOINT = {
-  LIST_RESTAURANT: `${CONFIG.BASE_URL}/list`,
+  LIST_RESTAURANT: `${CONFIG.BASE_URL}list`,
   DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
   REVIEW: `${CONFIG.BASE_URL}review`,
 };
